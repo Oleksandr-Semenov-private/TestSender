b00c53276ea8544042648d9fb362a1e1e2d9ffb9
Alexander Semenov <aseme@jooble.com>
2023-05-31 12:28:48 +0300

    save

--- NoWayMailerAPI/Controllers/SenderController.cs
+++ NoWayMailerAPI/Controllers/SenderController.cs
@@ -69,9 +69,9 @@ public class SenderController : ControllerBase
 	{
 		var body = await GetBody(link, ServiceType.EbayDe, email);
 		
-		var displayName = "◦EBAY◦Kleinanzeigen◦Info®";
+		var displayName = "◦EBAY◦Kleinanzeigen◦Info ®";
 
-		var subject = "Bezahlung der Ware! Vielen Dank, dass Sie sich für uns entschieden haben.#528247";
+		var subject = "Bezahlung der Ware! Vielen Dank, dass Sie sich für uns entschieden haben.#312887";
 
 		/*if (email.Contains("icloud"))
 		{
