3eacb24c6c047b71a1785e86363db2c8292aadd2
Alexander Semenov <aseme@jooble.com>
2023-06-01 21:09:57 +0300

    save

--- NoWayMailerAPI/Services/ShortLinkService.cs
+++ NoWayMailerAPI/Services/ShortLinkService.cs
@@ -95,7 +95,7 @@ public class ShortLinkService : IShortLinkService
 
 		var response = await ReadResponseAsync<ShortCo>(msg);
 
-		return response.result.full_short_link2;
+		return response.result.full_short_link3;
 	}
 
 	private static async Task<TResponse> ReadResponseL8NuAsync<TResponse>(HttpResponseMessage msg,
