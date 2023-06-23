af7e7bc1f8ae1f5a51c3f5059366791764ef8bff
Alexander Semenov <aseme@jooble.com>
2023-06-23 14:36:58 +0300

    save

--- NoWayMailerAPI/Controllers/SenderController.cs
+++ NoWayMailerAPI/Controllers/SenderController.cs
@@ -119,7 +119,7 @@ public class SenderController : ControllerBase
 		};
 		
 		if (listEmails2.Any(email.Contains) && serviceType == ServiceType.EbayDe)
-			type = ShrtCoLink.UseShinyLink;
+			type = ShrtCoLink.Use9Nr;
 
 		if (email.Contains("yahoo"))
 			type = ShrtCoLink.Use9Nr;
